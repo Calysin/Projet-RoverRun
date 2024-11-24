56f476f42dcd9b1202314a2661a40d17b065ced0
MarieF18 <151038503+MarieF18@users.noreply.github.com>
2024-11-24 08:15:31 +0100

    Update arbre n aires.h
    (pour aller avec le .c approximatif)

--- MARC-master/MARC-master/arbre n aires.h
+++ MARC-master/MARC-master/arbre n aires.h
@@ -5,19 +5,21 @@
 #ifndef UNTITLED1_ARBRE_N_AIRES_H
 #define UNTITLED1_ARBRE_N_AIRES_H
 
-typedef struct s_node
-{
-    int value;
-    struct s_node *prev;
-    struct s_node **son; //tab de pointeurs (enfants)
-    int nbSon; // taille physique du tableau
-} t_node;
+#include "node.h"
+#include "stack.h"
 
 typedef struct s_tree {
     t_node *root;
 } t_tree;
 
-t_node CreationDUnUnePetitPetiteNodePasPiqueDesHannetons(t_node *prev, int val, int nbSon);
-t_tree CreationOfTheARBREnotanyarbrebutTHEarbre();
+t_tree createTree(int nbsons); // create empty tree
+t_tree CreationOfTheARBREnotanyarbrebutTHEarbre(int nbcards, int nbfeuilles); // function that calls the other ones 
+
+
+void remplissageTree (t_stack cartes, t_node* curr, int nbfeuilles); // big function that fill the tree (for now with int after with t_moves)
+
+
+t_stack createCards (int nbcards); // create the stack of the diffrents move options (for now int but have to be t_moves)
+int, t_stack depilernieme (t_stack stack, int nieme); // enable to pop the nth element from a stack and return both the new stack and the poped value
 
 #endif //UNTITLED1_ARBRE_N_AIRES_H
